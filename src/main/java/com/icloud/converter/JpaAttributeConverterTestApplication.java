dcf2f5f373b8c879c12e40b131698dee6c70a423
hope <hope@gravylab.co.kr>
2022-05-12 21:58:10 +0900

    Initial commit

---
+++ src/main/java/com/icloud/converter/JpaAttributeConverterTestApplication.java
@@ -0,0 +1,13 @@
+package com.icloud.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAttributeConverterTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaAttributeConverterTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icloud/converter/JpaAttributeConverterTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icloud.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAttributeConverterTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
